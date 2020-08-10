aa4677898fdd59478c3ce237b9bd2fb561654dc6
Murilo B Alves <61016975+mbalves1@users.noreply.github.com>
2020-08-10 19:57:03 -0300

    Add files via upload

---
+++ mobile/src/Pages/Favorites/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { View, ScrollView } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+
+import PageHeader from '../../components/PageHeader';
+
+import styles from './styles'
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import AsyncStorage from '@react-native-community/async-storage';
+
+function Favorites() {
+
+    const [ favorites, setFavorites ] = useState([]); 
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => { 
+            if (response) {
+                const favoriedTeachers = JSON.parse(response);
+    
+                setFavorites(favoriedTeachers);
+            }
+        });
+    }
+
+    useFocusEffect(() => {
+        loadFavorites();
+    });
+
+    return (
+        <View style={styles.container}>
+            <PageHeader title='Meus Proffys favoritos' />
+        
+            <ScrollView style={styles.teacherList} contentContainerStyle={{paddingHorizontal: 16, paddingBottom: 16,}} >
+                {favorites.map((teacher: Teacher) => {
+                    return (
+                        <TeacherItem key={teacher.id} teacher={teacher} favorited />
+                    )
+                })}
+            </ScrollView>
+        </View>
+    )
+}
+
+export default Favorites;
